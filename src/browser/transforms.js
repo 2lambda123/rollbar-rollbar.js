7b323012886f6ca296736ce931415a2c565103e0
Restyled.io <commits@restyled.io>
2024-01-23 21:14:50 +0000

    Restyled by prettier

--- src/browser/transforms.js
+++ src/browser/transforms.js
@@ -1,9 +1,9 @@
-var _ = require('../utility');
-var errorParser = require('../errorParser');
-var logger = require('./logger');
+var _ = require("../utility");
+var errorParser = require("../errorParser");
+var logger = require("./logger");
 
 function handleDomException(item, options, callback) {
-  if (item.err && errorParser.Stack(item.err).name === 'DOMException') {
+  if (item.err && errorParser.Stack(item.err).name === "DOMException") {
     var originalError = new Error();
     originalError.name = item.err.name;
     originalError.message = item.err.message;
@@ -18,17 +18,21 @@ function handleItemWithError(item, options, callback) {
   item.data = item.data || {};
   if (item.err) {
     try {
-      item.stackInfo = item.err._savedStackTrace ||
-                       errorParser.parse(item.err, item.skipFrames);
+      item.stackInfo =
+        item.err._savedStackTrace ||
+        errorParser.parse(item.err, item.skipFrames);
 
       if (options.addErrorContext) {
         addErrorContext(item);
       }
     } catch (e) {
-      logger.error('Error while parsing the error object.', e);
+      logger.error("Error while parsing the error object.", e);
       try {
-        item.message = item.err.message || item.err.description ||
-                       item.message || String(item.err);
+        item.message =
+          item.err.message ||
+          item.err.description ||
+          item.message ||
+          String(item.err);
       } catch (e2) {
         item.message = String(item.err) || String(e2);
       }
@@ -54,31 +58,31 @@ function addErrorContext(item) {
 
 function ensureItemHasSomethingToSay(item, options, callback) {
   if (!item.message && !item.stackInfo && !item.custom) {
-    callback(new Error('No message, stack info, or custom data'), null);
+    callback(new Error("No message, stack info, or custom data"), null);
   }
   callback(null, item);
 }
 
 function addBaseInfo(item, options, callback) {
   var environment =
-      (options.payload && options.payload.environment) || options.environment;
+    (options.payload && options.payload.environment) || options.environment;
   item.data = _.merge(item.data, {
-    environment : environment,
-    level : item.level,
-    endpoint : options.endpoint,
-    platform : 'browser',
-    framework : 'browser-js',
-    language : 'javascript',
-    server : {},
-    uuid : item.uuid,
-    notifier : {name : 'rollbar-browser-js', version : options.version},
-    custom : item.custom
+    environment: environment,
+    level: item.level,
+    endpoint: options.endpoint,
+    platform: "browser",
+    framework: "browser-js",
+    language: "javascript",
+    server: {},
+    uuid: item.uuid,
+    notifier: { name: "rollbar-browser-js", version: options.version },
+    custom: item.custom,
   });
   callback(null, item);
 }
 
 function addRequestInfo(window) {
-  return function(item, options, callback) {
+  return function (item, options, callback) {
     var requestInfo = {};
 
     if (window && window.location) {
@@ -86,17 +90,16 @@ function addRequestInfo(window) {
       requestInfo.query_string = window.location.search;
     }
 
-    var remoteString = '$remote_ip';
+    var remoteString = "$remote_ip";
     if (!options.captureIp) {
       remoteString = null;
     } else if (options.captureIp !== true) {
-      remoteString += '_anonymize';
+      remoteString += "_anonymize";
     }
-    if (remoteString)
-      requestInfo.user_ip = remoteString;
+    if (remoteString) requestInfo.user_ip = remoteString;
 
     if (Object.keys(requestInfo).length > 0) {
-      _.set(item, 'data.request', requestInfo);
+      _.set(item, "data.request", requestInfo);
     }
 
     callback(null, item);
@@ -104,28 +107,28 @@ function addRequestInfo(window) {
 }
 
 function addClientInfo(window) {
-  return function(item, options, callback) {
+  return function (item, options, callback) {
     if (!window) {
       return callback(null, item);
     }
     var nav = window.navigator || {};
     var scr = window.screen || {};
-    _.set(item, 'data.client', {
-      runtime_ms : item.timestamp - window._rollbarStartTime,
-      timestamp : Math.round(item.timestamp / 1000),
-      javascript : {
-        browser : nav.userAgent,
-        language : nav.language,
-        cookie_enabled : nav.cookieEnabled,
-        screen : {width : scr.width, height : scr.height}
-      }
+    _.set(item, "data.client", {
+      runtime_ms: item.timestamp - window._rollbarStartTime,
+      timestamp: Math.round(item.timestamp / 1000),
+      javascript: {
+        browser: nav.userAgent,
+        language: nav.language,
+        cookie_enabled: nav.cookieEnabled,
+        screen: { width: scr.width, height: scr.height },
+      },
     });
     callback(null, item);
   };
 }
 
 function addPluginInfo(window) {
-  return function(item, options, callback) {
+  return function (item, options, callback) {
     if (!window || !window.navigator) {
       return callback(null, item);
     }
@@ -134,9 +137,9 @@ function addPluginInfo(window) {
     var cur;
     for (var i = 0, l = navPlugins.length; i < l; ++i) {
       cur = navPlugins[i];
-      plugins.push({name : cur.name, description : cur.description});
+      plugins.push({ name: cur.name, description: cur.description });
     }
-    _.set(item, 'data.client.javascript.plugins', plugins);
+    _.set(item, "data.client.javascript.plugins", plugins);
     callback(null, item);
   };
 }
@@ -158,23 +161,27 @@ function addBodyMessage(item, options, callback) {
   var custom = item.custom;
 
   if (!message) {
-    message = 'Item sent with null or missing arguments.';
+    message = "Item sent with null or missing arguments.";
   }
-  var result = {body : message};
+  var result = { body: message };
 
   if (custom) {
     result.extra = _.merge(custom);
   }
 
-  _.set(item, 'data.body', {message : result});
+  _.set(item, "data.body", { message: result });
   callback(null, item);
 }
 
 function stackFromItem(item) {
   // Transform a TraceKit stackInfo object into a Rollbar trace
   var stack = item.stackInfo.stack;
-  if (stack && stack.length === 0 && item._unhandledStackInfo &&
-      item._unhandledStackInfo.stack) {
+  if (
+    stack &&
+    stack.length === 0 &&
+    item._unhandledStackInfo &&
+    item._unhandledStackInfo.stack
+  ) {
     stack = item._unhandledStackInfo.stack;
   }
   return stack;
@@ -190,7 +197,7 @@ function addBodyTraceChain(item, options, callback) {
     traces.push(trace);
   }
 
-  _.set(item, 'data.body', {trace_chain : traces});
+  _.set(item, "data.body", { trace_chain: traces });
   callback(null, item);
 }
 
@@ -199,7 +206,7 @@ function addBodyTrace(item, options, callback) {
 
   if (stack) {
     var trace = buildTrace(item, item.stackInfo, options);
-    _.set(item, 'data.body', {trace : trace});
+    _.set(item, "data.body", { trace: trace });
     callback(null, item);
   } else {
     var stackInfo = item.stackInfo;
@@ -207,7 +214,7 @@ function addBodyTrace(item, options, callback) {
     var className = errorClass(stackInfo, guess[0], options);
     var message = guess[1];
 
-    item.message = className + ': ' + message;
+    item.message = className + ": " + message;
     addBodyMessage(item, options, callback);
   }
 }
@@ -220,7 +227,7 @@ function buildTrace(item, stackInfo, options) {
   var guess = errorParser.guessErrorClass(stackInfo.message);
   var className = errorClass(stackInfo, guess[0], options);
   var message = guess[1];
-  var trace = {exception : {'class' : className, message : message}};
+  var trace = { exception: { class: className, message: message } };
 
   if (description) {
     trace.exception.description = description;
@@ -243,18 +250,22 @@ function buildTrace(item, stackInfo, options) {
     for (i = 0; i < stack.length; ++i) {
       stackFrame = stack[i];
       frame = {
-        filename : stackFrame.url ? _.sanitizeUrl(stackFrame.url) : '(unknown)',
-        lineno : stackFrame.line || null,
-        method : (!stackFrame.func || stackFrame.func === '?')
-                     ? '[anonymous]'
-                     : stackFrame.func,
-        colno : stackFrame.column
+        filename: stackFrame.url ? _.sanitizeUrl(stackFrame.url) : "(unknown)",
+        lineno: stackFrame.line || null,
+        method:
+          !stackFrame.func || stackFrame.func === "?"
+            ? "[anonymous]"
+            : stackFrame.func,
+        colno: stackFrame.column,
       };
       if (options.sendFrameUrl) {
         frame.url = stackFrame.url;
       }
-      if (frame.method && frame.method.endsWith &&
-          frame.method.endsWith('_rollbar_wrapped')) {
+      if (
+        frame.method &&
+        frame.method.endsWith &&
+        frame.method.endsWith("_rollbar_wrapped")
+      ) {
         continue;
       }
 
@@ -306,29 +317,29 @@ function errorClass(stackInfo, guess, options) {
   } else if (options.guessErrorClass) {
     return guess;
   } else {
-    return '(unknown)';
+    return "(unknown)";
   }
 }
 
 function addScrubber(scrubFn) {
-  return function(item, options, callback) {
+  return function (item, options, callback) {
     if (scrubFn) {
       var scrubFields = options.scrubFields || [];
       var scrubPaths = options.scrubPaths || [];
       item.data = scrubFn(item.data, scrubFields, scrubPaths);
     }
     callback(null, item);
-  }
+  };
 }
 
 module.exports = {
-  handleDomException : handleDomException,
-  handleItemWithError : handleItemWithError,
-  ensureItemHasSomethingToSay : ensureItemHasSomethingToSay,
-  addBaseInfo : addBaseInfo,
-  addRequestInfo : addRequestInfo,
-  addClientInfo : addClientInfo,
-  addPluginInfo : addPluginInfo,
-  addBody : addBody,
-  addScrubber : addScrubber
+  handleDomException: handleDomException,
+  handleItemWithError: handleItemWithError,
+  ensureItemHasSomethingToSay: ensureItemHasSomethingToSay,
+  addBaseInfo: addBaseInfo,
+  addRequestInfo: addRequestInfo,
+  addClientInfo: addClientInfo,
+  addPluginInfo: addPluginInfo,
+  addBody: addBody,
+  addScrubber: addScrubber,
 };
